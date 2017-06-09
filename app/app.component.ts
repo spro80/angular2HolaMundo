549cc724afbfaa0e9d65269a0a6fa592da285f74
David Diaz <david.diaz@zweicom.com>
2017-06-09 15:12:07 -0400

    Se coloca titulo del sistema en app/app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,13 @@ import {Component} from 'angular2/core';
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
-    template: '<h1>Hola mundo con Angular 2 !! victorroblesweb.es</h1>'
+    //template: '<h1>Hola mundo con Angular 2 !! victorroblesweb.es</h1>'
+    templateUrl: 'app/view/inicio.html'
 })
  
 // Clase del componente donde iran los datos y funcionalidades
-export class AppComponent { }
+export class AppComponent { 
+
+    public tituloSistema:string = "Titulo del Sistema.";
+
+}
